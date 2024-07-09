ea98cd43a8d82b8d16f377ad34f2a1f8e420b42a
Jack <jackthor0608@gmail.com>
2024-07-09 19:07:17 -0400

    Update game.py

--- game.py
+++ game.py
@@ -22,19 +22,34 @@ class MancalaGame:
             y1 = 250
             x2 = x1 + 75
             y2 = y1 + 75
-            self.pits.append(self.canvas.create_oval(x1, y1, x2, y2, fill="white", outline="black"))
+            pit = self.canvas.create_oval(x1, y1, x2, y2, fill="white", outline="black")
+            self.pits.append(pit)
+            self.canvas.tag_bind(pit, "<Button-1>", lambda event, i=i: self.make_move(i))
         for i in range(6):
             x1 = 200 + (5 - i) * 100
             y1 = 50
             x2 = x1 + 75
             y2 = y1 + 75
-            self.pits.append(self.canvas.create_oval(x1, y1, x2, y2, fill="white", outline="black"))
+            pit = self.canvas.create_oval(x1, y1, x2, y2, fill="white", outline="black")
+            self.pits.append(pit)
+            self.canvas.tag_bind(pit, "<Button-1>", lambda event, i=i+7: self.make_move(i))
 
     def update_board(self):
+        self.canvas.delete("text")
         for i in range(14):
             x1, y1, x2, y2 = self.canvas.coords(self.pits[i])
             cx, cy = (x1 + x2) / 2, (y1 + y2) / 2
-            self.canvas.create_text(cx, cy, text=str(self.board[i]), font=("Arial", 18, "bold"))
+            self.canvas.create_text(cx, cy, text=str(self.board[i]), font=("Arial", 18, "bold"), tags="text")
+
+    def make_move(self, index):
+        stones = self.board[index]
+        self.board[index] = 0
+        pos = index
+        while stones > 0:
+            pos = (pos + 1) % 14
+            self.board[pos] += 1
+            stones -= 1
+        self.update_board()
 
 if __name__ == "__main__":
     root = tk.Tk()
